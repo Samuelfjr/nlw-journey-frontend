327954c7c80313214172e9df25ecdc64f8f6765e
Samuelfjr <devsamuelfjr@gmail.com>
2024-07-10 06:22:48 -0300

    feat: texto página inicial

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,20 @@
 export function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="max-w-3xl px-6">
+        <p className="text-lg text-zinc-300">
+          Convide seus amigos e planeje sua próxima viagem!
+        </p>
+
+        <div>
+        </div>
+
+        <p className="text-sm text-zinc-300">
+          Ao planejar sua viagem pela plann.er você automaticamente concorda{" "}
+          <br /> com nossos <a href="#">termos de uso</a> e{" "}
+          <a href="#"> políticas de privacidade</a>
+        </p>
+      </div>
+    </div>
+  );
 }
