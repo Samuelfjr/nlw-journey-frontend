4f79541e2143797901e01781e09c192ee01665ce
Samuelfjr <devsamuelfjr@gmail.com>
2024-07-10 06:53:17 -0300

    feat: input parcial

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,23 @@
 export function App() {
   return (
     <div className="flex items-center justify-center h-screen">
-      <div className="max-w-3xl px-6">
+      <div className="w-full max-w-3xl px-6 space-y-10">
         <p className="text-lg text-zinc-300">
           Convide seus amigos e planeje sua próxima viagem!
         </p>
 
-        <div>
+        <div className="flex items-center h-16 px-4 rounded-xl bg-zinc-900 shadow-shape">
+          <input
+            type="text"
+            placeholder="Para onde você vai??"
+            className="text-lg bg-transparent placeholder-zinc-400"
+          />
+          <input
+            type="text"
+            placeholder="Quando?"
+            className="text-lg bg-transparent placeholder-zinc-400"
+          />
+          <button>Continuar</button>
         </div>
 
         <p className="text-sm text-zinc-300">
