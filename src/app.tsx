9a4e0cd101e688391ff0ce22d19aa8383cf980b1
Samuel Fernandes Junior <sfernandes@vrtem022>
2024-07-12 15:15:44 -0300

    feat: estiliza botão

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,5 @@
+import { MapPin, Calendar, ArrowRight } from "lucide-react";
+
 export function App() {
   return (
     <div className="flex items-center justify-center h-screen">
@@ -7,17 +9,23 @@ export function App() {
         </p>
 
         <div className="flex items-center h-16 px-4 rounded-xl bg-zinc-900 shadow-shape">
-          <input
-            type="text"
-            placeholder="Para onde você vai?"
-            className="text-lg bg-transparent placeholder-zinc-400"
+          <div className="flex items-center gap-2 flex-1">
+            <MapPin className="size-5 text-zinc-400"/>
+            <input
+              type="text"
+              placeholder="Para onde você vai?"
+              className="text-lg bg-transparent placeholder-zinc-400"
+            />
+          </div>
+          <div className="flex items-center gap-2">
+            <Calendar className="size-5 text-zinc-400"/>
+            <input
+              type="text"
+              placeholder="Quando?"
+              className="text-lg bg-transparent placeholder-zinc-400"
           />
-          <input
-            type="text"
-            placeholder="Quando?"
-            className="text-lg bg-transparent placeholder-zinc-400"
-          />
-          <button>Continuar</button>
+          </div>
+          <button className="bg-lime-400 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400">Continuar <ArrowRight className="size-5 text-lime-950"/></button>
         </div>
 
         <p className="text-sm text-zinc-300">
